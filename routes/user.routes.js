e20a2a83f341e09287f38850545bcd4c352e803f
Abhiram Suradkar <110467020+abhi32GBram@users.noreply.github.com>
2023-05-07 17:36:32 +0530

    Insertion Timeout Error - Progress Halted

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { UserModel } = require("../models/UserModel");
 const bcrypt = require("bcrypt");
 
-
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res) => {
@@ -15,23 +14,18 @@ userRouter.post("/register", async (req, res) => {
     if (err) return res.send({ message: "OOPS !! SOMETHING WENT WRONG", status: 0 });
     try {
       let user = new UserModel({ name, email, password: hash });
-      await user.save();
+      await user.save({ wtimeout: 15000 });
       res.send({
         message: "USER CREATED SUCCESSFULLY !! ",
         status: 1,
       });
     } catch (error) {
       res.send({
-        message: err.message,
+        message: error.message,
         status: 0,
       });
     }
   });
 });
 
- 
 module.exports = { userRouter };
-
-// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-//     // Store hash in your password DB.
-// });
